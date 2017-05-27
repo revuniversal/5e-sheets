b98ba4a52b896d6ca429fb2c70511db394ec3f3d
Rev Herr <revuniversal@gmail.com>
2017-05-27 16:22:45 -0500

    feat(sheet): adjust character name spacing and alignment

--- src/character/sheet/personal/PersonalInfo.js
+++ src/character/sheet/personal/PersonalInfo.js
@@ -12,59 +12,75 @@ const Container = styled.div`
 
 const CharacterName = styled.div`
   text-align: left;
-  font-size: 2.2em;
+  font-size: 1.6em;
   width: 30%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-self: stretch;
+  align-content: center;
 `;
 
 const InfoBlock = styled.div`
   width: 70%;
-  border: 1px solid black;
+  border: 1px solid ${p => p.theme.colors.primary};
   border-radius: 0.5em;
+  font-size: 1.2em;
 `;
 
-const ValueRow = styled.div`
+const Values = styled.div`
   text-align: left;
   padding: 0.5em 12px 0.1em;
   height: 1.4em;
   line-height: 1.8em;
   font-size: 1.4em;
 `;
-const LabelRow = styled.div`
+const Labels = styled.div`
   text-align: left;
-  padding: 0em 12px 0.5em;
-  margin-bottom: 0.2em;
+  padding: 2px 12px 0.5em;
+  margin: 0 0 1px;
   height: 0.8em;
-  line-height: 0.5em;
-  font-size: 0.8em;
-  border-top: 1px solid black;
+  line-height: 0.8em;
+  font-size: 8px;
+  border-top: 1px solid ${p => p.theme.colors.primary};
+  color: ${p => p.theme.colors.primary};
 `;
 
 const PersonalInfo = ({ character }) => (
   <Container>
-    <CharacterName>{character.characterName}</CharacterName>
+    <CharacterName>
+      <div>
+        <Values>
+          {character.characterName}
+        </Values>
+        <Labels>
+          Character Name
+        </Labels>
+      </div>
+    </CharacterName>
     <InfoBlock>
-      <ValueRow>
+      <Values>
         <Cell columns={6}>
           Level {character.stats.level} {character.stats.class}
         </Cell>
         <Cell columns={6}>{character.playerName}</Cell>
-      </ValueRow>
-      <LabelRow>
+      </Values>
+      <Labels>
         <Cell columns={6}>Level and Class</Cell>
         <Cell columns={6}>Player Name</Cell>
-      </LabelRow>
-      <ValueRow>
+      </Labels>
+      <Values>
         <Cell columns={3}>{character.background.name}</Cell>
         <Cell columns={3}>{character.race}</Cell>
         <Cell columns={3}>{character.stats.experience}</Cell>
         <Cell columns={3}>{character.stats.nextLevel}</Cell>
-      </ValueRow>
-      <LabelRow>
+      </Values>
+      <Labels>
         <Cell columns={3}>Background</Cell>
         <Cell columns={3}>Race</Cell>
         <Cell columns={3}>Experience</Cell>
         <Cell columns={3}>Next Level</Cell>
-      </LabelRow>
+      </Labels>
     </InfoBlock>
   </Container>
 );
