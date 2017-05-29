011ae2146db6e7b2cc6c06f43d55e3ad8f458285
Rev Herr <revuniversal@gmail.com>
2017-05-29 15:33:52 -0500

    feat: remove unnecessary global app styles

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React, {Component} from 'react'
-import CharacterSheet from './character/sheet/CharacterSheet'
-import './App.css'
+import React from 'react';
+import CharacterSheet from './character/sheet/CharacterSheet';
 
-class App extends Component {
-  render() {
-    const character = this.props.character
-    return (
-      <div className="container">
-        <div className="App">
-          <CharacterSheet character={character} />
-        </div>
-      </div>
-    )
-  }
-}
+const App = ({ character }) => (
+  <div className="container">
+    <div className="App">
+      <CharacterSheet character={character} />
+    </div>
+  </div>
+);
 
-export default App
+export default App;
