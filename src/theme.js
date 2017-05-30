307d2a496381bb8a52bfcf335b6fe729f7ba2fdd
Rev Herr <revuniversal@gmail.com>
2017-05-30 17:43:34 -0500

    refactor(theme): simplify theme

---
+++ src/theme.js
@@ -0,0 +1,25 @@
+// const pastelGray = 'rgba(214, 206, 202, 1)';
+const oldRose = 'rgba(185, 139, 130, 1)';
+const parrotPink = 'rgba(228, 149, 158, 1)';
+const darkSlateGray = 'rgba(55, 81, 95, 1)';
+// const licorice = 'rgba(31, 8, 18, 1)';
+const background = 'white';
+const foreground = 'black';
+const primary = darkSlateGray;
+const accent1 = oldRose;
+const accent2 = parrotPink;
+
+const colors = {
+  background,
+  foreground,
+  primary,
+  accent1,
+  accent2
+};
+
+export default {
+  colors,
+  bg: colors.background,
+  fg: colors.foreground,
+  muted: '#CCCCCC'
+};
--- src/theme/index.js
+++
@@ -1,8 +0,0 @@
-import colors from './colors';
-
-export default {
-  colors,
-  bg: colors.background,
-  fg: colors.foreground,
-  muted: '#CCCCCC'
-};
