f9dddba34e6954d9931ef73d6276ce2ef80d5832
Rev Herr <revuniversal@gmail.com>
2019-07-01 10:49:08 -0500

    style: formatting

--- src/character/sheet/get-modifier.js
+++ src/character/sheet/get-modifier.js
@@ -1,10 +1,6 @@
 function getModifier(score) {
-  let modifier = Math.floor((score-10) / 2)
-  return (modifier === 0)
-    ? 0
-    : (modifier > 0)
-    ? `+${modifier}`
-    : modifier
+  let modifier = Math.floor((score - 10) / 2);
+  return modifier === 0 ? 0 : modifier > 0 ? `+${modifier}` : modifier;
 }
 
-export default getModifier
+export default getModifier;
