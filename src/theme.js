cb62d32eb80d28d8a00f11bcc328473beb731811
Rev Herr <revuniversal@gmail.com>
2019-07-01 10:48:44 -0500

    Style: reduce theme boilerplate

--- src/theme.js
+++ src/theme.js
@@ -1,20 +1,13 @@
-// const pastelGray = 'rgba(214, 206, 202, 1)';
 const oldRose = 'rgba(185, 139, 130, 1)';
 const parrotPink = 'rgba(228, 149, 158, 1)';
 const darkSlateGray = 'rgba(55, 81, 95, 1)';
-// const licorice = 'rgba(31, 8, 18, 1)';
-const background = 'white';
-const foreground = 'black';
-const primary = darkSlateGray;
-const accent1 = oldRose;
-const accent2 = parrotPink;
 
 const colors = {
-  background,
-  foreground,
-  primary,
-  accent1,
-  accent2
+  background: 'white',
+  foreground: 'black',
+  primary: darkSlateGray,
+  accent1: oldRose,
+  accent2: parrotPink
 };
 
 export default {
